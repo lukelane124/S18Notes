35a9bf6f872fb389ffccde8edc0a7cb4a19f2fba
Tommy Lane <lukelane124@Gmail.com>
2018-02-27 09:24:44 -0600

    Updates changes so sink.sh into repo

--- sink.sh
+++ sink.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
+shopt -s expand_aliases
+alias pandoc="/home/CAE/tllane42/USERINSTALL/pandoc-2.1.1/bin/pandoc"
+alias pandoc-citeproc="/home/CAE/tllane42/USERINSTALL/pandoc-2.1.1/bin/pandoc-citeproc"
+
+pandoc WorldCiv/WorldCiv.org -o WorldCiv/WorldCiv.pdf
+pandoc WorldCiv/WorldCiv.org -o WorldCiv/WorldCiv.html
+pandoc Telecom/telecom.org -o Telecom/telecom.pdf
+pandoc Telecom/telecom.org -o Telecom/telecom.html
+pandoc CmpDesign/CmpDesign.org -o CmpDesign/CmpDesign.pdf
+pandoc CmpDesign/CmpDesign.org -o CmpDesign/CmpDesign.html
+pandoc Distributed/Distributed.org -o Distributed/Distributed.pdf
+pandoc Distributed/Distributed.org -o Distributed/Distributed.html
+
+
+
 
